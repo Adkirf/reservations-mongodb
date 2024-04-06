7253afff2334333794e6bc8d285625b1f702d31d
youba-experiences <social@youba.io>
2024-04-06 18:06:09 +0100

    add mongose single connection

--- src/utils/mydb.js
+++ src/utils/mydb.js
@@ -1,26 +1,42 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
 const mongoose = require("mongoose");
 
-// MongoDB connection URI
-const uri = process.env.MONGODB_URI;
+// Assuming MONGODB_URI is set in your environment variables
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// Function to connect to MongoDB
-const dbConnect = async () => {
-  try {
-    await mongoose.connect(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+// Use a global variable to store the cache
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
+}
+
+let cached = global.mongoose;
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn; // Use existing database connection if already connected
   }
-};
+  if (!cached.promise) {
+    // Initiate connection only if there isn't a pending connection promise
+    const opts = {
+      bufferCommands: false, // Specific Mongoose options can be defined here
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise; // Wait for the connection to be established
+  } catch (e) {
+    cached.promise = null; // Reset promise to allow retrying in case of failure
+    throw e;
+  }
 
-// Export the function to connect to MongoDB
+  return cached.conn;
+}
+
 module.exports = dbConnect;
